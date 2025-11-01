2ac52948d9b7e57c5ebcc94f581afa71d296aed7
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2025-11-02 00:45:20 +0530

    Restructuring the assets

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,4 @@
-import AddToCartIcon from "../assets/images/icon-add-to-cart.svg";
+import AddToCartIcon from "../../assets/images/icon-add-to-cart.svg";
 
 export default function ProductCard({
   imgSrc,
@@ -8,24 +8,27 @@ export default function ProductCard({
   onAddToCart,
 }) {
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col gap-3">
       <div className="relative">
         <img src={imgSrc} alt={name} className="w-full h-auto rounded-lg" />
-        <div className="w-full">
-          <button onClick={onAddToCart} className="">
+        <div className="w-full flex justify-center">
+          <button
+            onClick={onAddToCart}
+            className="cursor-pointer mt-[-22px] border-[1.7px] border-[#C8A59A] bg-[white] text-[#2E0D09] px-4 py-2 rounded-full font-medium"
+          >
             <img
               src={AddToCartIcon}
               alt="Add to cart"
               className="inline-block mr-2"
             />
-            <span>Add to cart</span>
+            <span>Add to Cart</span>
           </button>
         </div>
       </div>
       <div className="flex flex-col gap-1">
-        <h3 className="text-[#D9BFB6]">{category}</h3>
-        <h2 className="text-[#2E0D09]">{name}</h2>
-        <h2 className="text-[#D6452A]">${price}</h2>
+        <h3 className="text-[#C8A59A] text-[14px]">{category}</h3>
+        <h2 className="text-[#2E0D09] text-[16px]">{name}</h2>
+        <h2 className="text-[#D6452A] text-[18px]">${price}</h2>
       </div>
     </div>
   );
--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -2,7 +2,7 @@ import ProductCard from "./ProductCard";
 
 export default function ProductsList({ products }) {
   return (
-    <div className="flex flex-col gap-6 w-[60vw]">
+    <div className="flex flex-col gap-6 w-[55vw]">
       <h1 className="text-4xl font-bold">Desserts</h1>
       <div className="grid grid-cols-3 gap-5">
         {products.map(({ image, ...rest }) => (
